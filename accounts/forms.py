8c8742afb3fa45e44c18209f61cbd6ddd85704f5
AliAkbarBadri <aabadri74@gmail.com>
2025-03-21 15:02:59 +0330

    create project and add basic models

---
+++ accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import AdminUserCreationForm, UserChangeForm
+from .models import CustomUser
+
+
+class CustomUserCreationForm(AdminUserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = AdminUserCreationForm.Meta.fields + ("name",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = UserChangeForm.Meta.fields
